d585a338807394716584787fe6dac6ccf09e19e1
eroncardoso <66843643+eroncardoso@users.noreply.github.com>
2021-08-22 16:52:23 -0300

    initial-commit

---
+++ src/main/java/com/example/botcreator/BotCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.botcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/botcreator/BotCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
